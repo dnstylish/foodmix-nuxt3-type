5cfaa6cb28512f11bf7bb54f8a2fa825f424007c
Yuan <dnstylish@gmail.com>
2022-02-20 15:57:10 +0700

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+import { defineNuxtConfig } from 'nuxt3'
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+    css: ['@/assets/css/main.css'],
+    modules: [
+        '@nuxtjs/tailwindcss'
+    ]
+})
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="opacity-0">{{ counter }}</div>
+    <div @click="tangLen">Click me</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "@vue/runtime-core";
+
+const counter = ref(0)
+
+function tangLen(): void {
+  counter.value++
+}
+
+</script>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  name: "HomePage"
+})
+</script>
